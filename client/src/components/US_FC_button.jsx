7e20b29152155b13650f356c6b326f073664bff2
Adeline <149504531+kna1703@users.noreply.github.com>
2024-04-10 10:47:50 +0200

    create componements button

---
+++ client/src/components/US_FC_button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function US_FC_button() {
+    return (
+        <div>
+            <label className="country"></label>
+            <button>Search</button>
+        </div>
+      
+    );
+  }
+  
+  export default US_FC_button;
