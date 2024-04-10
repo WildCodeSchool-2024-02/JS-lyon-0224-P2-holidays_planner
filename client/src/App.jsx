6be34451f3725d7f7a677ad56b31238421599014
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-10 16:08:38 +0200

    add fetch on appjsx and modified result.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,39 @@
 import "./App.css";
 import US_FC_Result from './components/US_FC_Result.jsx';
+import { useState } from "react";
+import { useEffect } from "react";
 // import US_FC_button from './components/US_FC_button.jsx';
 // import Header from './components/header.jsx';
-// mettez ton path button et header ici 
+
 
 function App() {
+
+  const [data, setData] = useState([]); // avecsetData, on change data chque fois 
+
+  useEffect(() => { //Dire react "finir qu'un seul fois pour nous donner les data de API avec useEffect"
+    fetch("https://date.nager.at/api/v3/NextPublicHolidays/FR")
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch(() => alert("error"));
+  }, [])
+
+  console.log(data);
+
   return (
     <div>
       <main>
         {/* <Header />
         <US_FC_button /> */}
         <US_FC_Result />
+        <div>
+          {data.map((holiday, index) => (
+            <div key={index}>
+              {holiday.date} - {holiday.name} - {holiday.localName}
+            </div>
+          ))}
+        </div>
       </main>
+
     </div>
   );
 }
