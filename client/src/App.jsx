b30c87e4f2896c8bc995a5218a0e34c2e01ade1d
Bastien Fournier <bastien.fournier06@gmail.com>
2024-04-10 15:37:13 +0200

    add file header.css and components header.jsx for navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
+// import './services/filter_country.css'
 import US_FC_Result from './components/US_FC_Result.jsx';
-// import US_FC_button from './components/US_FC_button.jsx';
-// import Header from './components/header.jsx';
+import US_FC_button from './components/US_FC_button.jsx';
+import Header from './components/header.jsx';
 // mettez ton path button et header ici 
 
 function App() {
   return (
     <div>
       <main>
-        {/* <Header />
+        <Header/>
         <US_FC_button /> */}
         <US_FC_Result />
       </main>
