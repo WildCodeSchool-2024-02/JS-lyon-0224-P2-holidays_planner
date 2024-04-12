975e2e4eb2fffdf59efe0d5ab7431dfcf7fbaf89
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-12 09:26:17 +0200

    modifed appjx/result.jsx/bothcss and add one image travel

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
+import styles from "./footer.module.css";
+import travel from "../../assets/images/travel.png";
+
 function Footer() {
-    return (
-      <div>
-        <p>footer</p>
-      </div>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <div>
+      <p className={styles.footerText}>
+        <img src={travel} />
+        By Holidays Planner
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
