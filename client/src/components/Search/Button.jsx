4d11d28cbbba6ae54d4914436b07ac54be4ef179
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-12 15:10:49 +0200

    fix prettier

--- client/src/components/Search/Button.jsx
+++ client/src/components/Search/Button.jsx
@@ -1,11 +1,11 @@
-import styles from "./button.module.css"
+import styles from "./button.module.css";
 
 function Button() {
   return (
     <div className={styles.blocSearch}>
       <label className={styles.title}>
         Choose your country
-        <input type="text" className={styles.country} minLength="2"/>
+        <input type="text" className={styles.country} minLength="2" />
         <button type="button" className={styles.search}>
           Search
         </button>
