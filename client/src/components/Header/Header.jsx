c26e1a435c73497a29b42f22627af8ed81feaa4c
Bastien Fournier <bastien.fournier06@gmail.com>
2024-04-16 14:40:12 +0200

    update navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,30 +5,28 @@ function Header() {
   return (
     <nav className={styles.navBar}>
       <img src={Logo} alt="logo earth" className={styles.logoPlanet} />
-      <div className={styles.divClass}>
-        <ul className={styles.navBarLinks}>
-          <li className={styles.navBarItem}>
-            <a href="/" className={styles.navBarLink}>
-              Homepage
-            </a>
-          </li>
-          <li className={styles.navBarItem}>
-            <a href="/" className={styles.navBarLink}>
-              Filter country
-            </a>
-          </li>
-          <li className={styles.navBarItem}>
-            <a href="/" className={styles.navBarLink}>
-              Filter period
-            </a>
-          </li>
-          <li className={styles.navBarItem}>
-            <a href="/" className={styles.navBarLink}>
-              About us
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className={styles.navBarLinks}>
+        <li className={styles.navBarItem}>
+          <a href="/" className={styles.navBarLink}>
+            Homepage
+          </a>
+        </li>
+        <li className={styles.navBarItem}>
+          <a href="/" className={styles.navBarLink}>
+            Filter country
+          </a>
+        </li>
+        <li className={styles.navBarItem}>
+          <a href="/" className={styles.navBarLink}>
+            Filter period
+          </a>
+        </li>
+        <li className={styles.navBarItem}>
+          <a href="/" className={styles.navBarLink}>
+            About us
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
