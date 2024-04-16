54381cd229e9c0187ed41a6c34e12079fcba0e90
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-16 15:40:30 +0200

    delete const inside of home button

--- client/src/components/Home/HomeButton/HomeButton.jsx
+++ client/src/components/Home/HomeButton/HomeButton.jsx
@@ -1,15 +1,9 @@
 // import styles from "./homebutton.module.css";
 
 function HomeButton() {
-  const [] 
-  const []
-  const []
-
   return (
     <div>
-      <button></button>
-      <button></button>
-      <button></button>
+      <p>toto</p>
     </div>
   );
 }
