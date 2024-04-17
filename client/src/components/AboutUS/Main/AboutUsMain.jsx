db7616e33b92471710415010257fc5d1b7056e5d
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-17 11:13:08 +0200

    "Add avatar image on AboutUS"

--- client/src/components/AboutUS/Main/AboutUsMain.jsx
+++ client/src/components/AboutUS/Main/AboutUsMain.jsx
@@ -1,20 +1,21 @@
-
+import styles from 
+import Avatar from "../../../assets/images/avatar.png"
 
 function AboutUs() {
     return (
 <div className={styles.aboutUs}> 
 <section className={styles.profileAdeline}>
-<img alt="imageAdeline" src={}></img>
+<img alt="imageAdeline" src={Avatar}></img>
 <p>lorem </p> 
 </section>  
 
 <section className={styles.profileBastien}>
-<img alt="imageBastien " src={}></img> 
+<img alt="imageBastien " src={Avatar}></img> 
 <p>lorem</p> 
 </section>
 
 <section className={styles.profileKana}> 
-<img alt="imageKana" src={}></img>
+<img alt="imageKana" src={Avatar}></img>
 <p> lorem </p> 
 </section >
 
