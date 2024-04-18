174bdec8a4623b0812a41a93e759990712926f30
Bastien Fournier <bastien.fournier06@gmail.com>
2024-04-18 12:22:23 +0200

    fix css homepage

--- client/src/components/Home/Homemain/Title.jsx
+++ client/src/components/Home/Homemain/Title.jsx
@@ -1,9 +1,17 @@
 import styles from "./title.module.css";
+import logo from "../../../assets/images/planet.png";
 
 function Title() {
   return (
     <div className={styles.divClass}>
-      <h1 className={styles.title}>Find your public holidays</h1>
+      <h1 className={styles.title}>
+        {" "}
+        <span className={styles.findYour}>Find your public</span>{" "}
+        <span className={styles.holidays}> holidays </span>
+      </h1>
+      <div className={styles.image}>
+        <img className={styles.logo} src={logo} alt="planet" />
+      </div>
     </div>
   );
 }
