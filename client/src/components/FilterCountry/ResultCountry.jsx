ada335974dca846bc35c83b5df8968af4606e8a9
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-23 16:16:39 +0200

    add button filterby period on home page and add function to  delete double event and date for the result

--- client/src/components/FilterCountry/ResultCountry.jsx
+++ client/src/components/FilterCountry/ResultCountry.jsx
@@ -3,15 +3,22 @@ import styles from "./resultcountry.module.css";
 
 function ResultCountry({ holidays }) {
   return (
-    <section className={styles.resultPage}>
-      <div>
+    <section className={styles.partResult}>
+      <div className={styles.resultDate}>
         <p className={styles.date}>Date</p>
+        {holidays.map((holiday, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={holiday.date + index}>
+            <p className={styles.resultsDate}>{holiday.date}</p>
+          </div>
+        ))}
+      </div>
+      <div className={styles.resultName}>
         <p className={styles.name}>Name</p>
-        {holidays.map((holiday) => (
-          <div key={holiday.date}>
-            <p className={styles.results}>
-              {holiday.date} {holiday.name}
-            </p>
+        {holidays.map((holiday, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={holiday.name + index}>
+            <p className={styles.resultsName}>{holiday.name}</p>
           </div>
         ))}
       </div>
