be8a7bd3ed23c327db3fcaa9061d6a34e28105a6
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-24 10:13:58 +0200

    add calendar

--- client/src/components/FilterCountrYear/FilterCountryYear.jsx
+++ client/src/components/FilterCountrYear/FilterCountryYear.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import SearchYear from "./SearchYear";
 import Resultyear from "./ResultYear";
 
 function FilterCountryYear() {
+  const [date, setDate] = useState(new Date());
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
+
   const [dataYear, setDataYear] = useState([]);
   const [year, setYear] = useState("");
   const [countryCodeYear, setCountryCodeYear] = useState("");
@@ -18,7 +25,21 @@ function FilterCountryYear() {
   return (
     <div>
       <SearchYear setYear={setYear} setCountryCodeYear={setCountryCodeYear} />
-      <Resultyear ataYear={dataYear} />
+      <Calendar
+        onChange={onChange}
+        value={date}
+        locale="en-UK"
+        onClickyear={(value, event) => alert("Clicked year: ", value)}
+        tileContent={({ date, view }) => {
+          const formattedDate = date.toISOString().split("T")[0]; //カレンダーから取り出す日付は、T以下の時刻などは必要とせず、年月日だけを取り出したい
+          const holiday = dataYear.find(
+            (holiday) => holiday.date === formattedDate
+          ); // APIデータの中はリストであり、リストの中のオブジェクトをholidayと呼ぶ。anotherdata（＝検索結果）とholidayの中のDateは先程指定したフォーマットと一緒になるはず。という前提をここで指定する
+
+          return view === "month" && holiday ? holiday.name : null; // 先程指定したフォーマットに一致している場合、結果は名前を表示する
+        }}
+      />
+      <Resultyear dataYear={dataYear} />
     </div>
   );
 }
