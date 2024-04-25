b966463b862be80793be77b2fd4cd53f844b406e
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-04-25 14:38:57 +0200

    fix eslint

--- client/src/components/Common/Header/Header.jsx
+++ client/src/components/Common/Header/Header.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import styles from "./header.module.css";
 import logo from "../../../assets/images/logo-lazy-day.png";
 import HomeLogo from "../../../assets/images/home.svg";
@@ -8,54 +7,37 @@ import CalendarLogo from "../../../assets/images/calendar.svg";
 import TeamLogo from "../../../assets/images/team.svg";
 
 function Header() {
-  const [burger, setBurger] = useState(false);
-  const changeBurger = () => setBurger(!burger);
-
   return (
-    <div className={styles.hamburger}>
-      <div>
-        <button
-          className={styles.buttonBurger}
-          type="button"
-          onClick={changeBurger}
-        >
-          {burger === true ? <FaTimes size={30} /> : <FaBars size={30} />}
-        </button>
-      </div>
-
-      {burger === true && (
-        <nav className={styles.navBar}>
+    <nav className={styles.navBar}>
+      <Link className={styles.navBarLink} to="/">
+        <img src={logo} alt="logo lazy day" className={styles.logo} />
+      </Link>
+      <ul className={styles.navBarLinks}>
+        <li>
           <Link className={styles.navBarLink} to="/">
-            <img src={logo} alt="logo lazy day" className={styles.logo} />
+            <img src={HomeLogo} alt="homelogo" /> Home
           </Link>
-          <ul className={styles.navBarLinks}>
-            <li>
-              <Link className={styles.navBarLink} to="/">
-                <img src={HomeLogo} alt="homelogo" /> Home
-              </Link>
-            </li>
-            <li>
-              <Link className={styles.navBarLink} to="/filtercountry">
-                <img src={EarthLogo} alt="earthlogo" />
-                Filter By Country
-              </Link>
-            </li>
-            <li>
-              <Link className={styles.navBarLink} to="/filtercountryyear">
-                <img src={CalendarLogo} alt="calendarlogo" />
-                Filter By Year and Country
-              </Link>
-            </li>
-            <li>
-              <Link className={styles.navBarLink} to="/aboutus">
-                <img src={TeamLogo} alt="teamlogo" />
-                About Us
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </div>
+        </li>
+        <li>
+          <Link className={styles.navBarLink} to="/filtercountry">
+            <img src={EarthLogo} alt="earthlogo" />
+            Filter By Country
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.navBarLink} to="/filtercountryyear">
+            <img src={CalendarLogo} alt="calendarlogo" />
+            Filter By Year and Country
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.navBarLink} to="/aboutus">
+            <img src={TeamLogo} alt="teamlogo" />
+            About Us
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
