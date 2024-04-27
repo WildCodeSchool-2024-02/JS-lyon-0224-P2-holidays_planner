dd040659841ecdda18ed6173d44676764f143cc2
Adeline <149504531+kna1703@users.noreply.github.com>
2024-04-27 09:59:01 +0200

    prettier ok

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -6,26 +6,34 @@ import Kana from "../../assets/images/Kana.jpg";
 function AboutUs() {
   return (
     <div>
-         <h1>About our team</h1>
-         <div className={styles.aboutUs}>
-      <span className={styles.Adeline}>
-      <img alt="Portrait of Adeline" src={Adeline} className={styles.photo} />
-      <h3>Adeline</h3>
-      <h4>Developer Web Junior</h4>
-      </span>
+      <h1>About our team</h1>
+      <div className={styles.aboutUs}>
+        <span className={styles.Adeline}>
+          <img
+            alt="Portrait of Adeline"
+            src={Adeline}
+            className={styles.photo}
+          />
+          <h3>Adeline</h3>
+          <h4>Developer Web Junior</h4>
+        </span>
 
-      <span className={styles.Bastien}>
-      <img alt="Portrait of Bastien" src={Bastien} className={styles.photo}/>
-      <h3>Bastien</h3>
-      <h4>Developer Web Junior</h4>
-      </span>
+        <span className={styles.Bastien}>
+          <img
+            alt="Portrait of Bastien"
+            src={Bastien}
+            className={styles.photo}
+          />
+          <h3>Bastien</h3>
+          <h4>Developer Web Junior</h4>
+        </span>
 
-      <span className={styles.Kana}>
-      <img alt="Portrait of Kana" src={Kana} className={styles.photo} />
-      <h3>Kana</h3>
-      <h4>Developer Web Junior</h4>
-      </span>
-    </div>
+        <span className={styles.Kana}>
+          <img alt="Portrait of Kana" src={Kana} className={styles.photo} />
+          <h3>Kana</h3>
+          <h4>Developer Web Junior</h4>
+        </span>
+      </div>
     </div>
   );
 }
